f0e956449faff6db5381fdcb42b5e82141a34359
kowabunga314 <dougfrost314@gmail.com>
2020-07-18 09:08:11 -0500

    added session to auth dependency and updated requirements.txt with alembic stuff

--- app/auth/api.py
+++ app/auth/api.py
@@ -8,7 +8,7 @@ from .crypt import authenticate_user, get_current_active_user, create_access_tok
 from .hash import verify_password, get_password_hash
 from .schema import Token, TokenData
 from app.config import ACCESS_TOKEN_EXPIRE_MINUTES
-from app.database import SessionLocal
+from app.database import SessionLocal, get_db
 from app.user.schema import UserBase, UserCreate
 from app.user.crud import get_user, get_user_by_username
 
@@ -17,18 +17,8 @@ from app.user.crud import get_user, get_user_by_username
 # Create router
 router = APIRouter()
 
-def get_db():
-    db = SessionLocal()
-    try:
-        yield db
-    finally:
-        db.close()
-
-
-
 @router.post("/token", response_model=Token)
-async def login_for_access_token(form_data: OAuth2PasswordRequestForm = Depends()):
-    session = get_db()
+async def login_for_access_token(form_data: OAuth2PasswordRequestForm = Depends(), session: SessionLocal = Depends(get_db)):
     user = authenticate_user(session, form_data.username, form_data.password)
     if not user:
         raise HTTPException(
