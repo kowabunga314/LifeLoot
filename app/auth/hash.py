ff5b7ea83531d0f02e3ff55faa18590fe5e177bf
kowabunga314 <dougfrost314@gmail.com>
2020-07-18 09:12:09 -0500

    added note to hash.py to justify that file's existence

--- app/auth/hash.py
+++ app/auth/hash.py
@@ -1,3 +1,8 @@
+"""
+This file is used to hash and unhash passwords. It can be used outside
+    of the auth package so this functionality is included as its own
+    file in order to avoid cyclical imports with other modules.
+"""
 from passlib.context import CryptContext
 
 
