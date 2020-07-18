aa81dcbe990583556b5b61c609d17df5282d268e
kowabunga314 <dougfrost314@gmail.com>
2020-07-18 14:39:31 -0500

    added user lookup endpoint and corresponding tests.

--- app/user/test_user.py
+++ app/user/test_user.py
@@ -23,6 +23,26 @@ def test_create_user():
         "active": True
     }
     context['user_id'] = response.json()['id']
+    context['user_username'] = response.json()['username']
+    context['user_email'] = response.json()['email']
+
+def test_get_user_by_id():
+    response = client.get(f"/users/{context.get('user_id', 1)}")
+    assert response.status_code == 404
+
+def test_get_user_by_username():
+    response = client.get(f"/users/{context.get('user_username', 'wumbo')}")
+    assert response.status_code == 200
+    assert response.json() == {
+        "email": "foo@bar",
+        "username": "wumbo",
+        "id": 1,
+        "active": True
+    }
+
+def test_get_user_by_email():
+    response = client.get(f"/users/{context.get('user_email', 'foo@bar')}")
+    assert response.status_code == 404
 
 def test_read_users():
     response = client.get("/users/")
