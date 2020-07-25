65ae592a23126fb85c9f921116bd3ba9c0563d45
kowabunga314 <dougfrost314@gmail.com>
2020-07-24 19:08:06 -0500

    Added todo in auth api for adding refresh tokens

--- app/auth/api.py
+++ app/auth/api.py
@@ -31,4 +31,7 @@ async def login_for_access_token(form_data: OAuth2PasswordRequestForm = Depends(
             headers={"WWW-Authenticate": "Bearer"},
         )
     access_token = create_access_token(data={"sub": user.username})
+
+    # TODO: Add refresh tokens to authentication header
+
     return {"access_token": access_token, "token_type": "bearer"}
